6bc7e813c1a26396c813b779fc665b5c3f729e90
ken0901 <ken5260715@gmail.com>
2021-09-17 22:36:51 -0700

    Basic setting

---
+++ src/test/java/com/ken/app/TestAppApplication.java
@@ -0,0 +1,13 @@
+package com.ken.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestAppApplication {
+
+    @Test
+    void contextLoad(){
+
+    }
+}
